982103daf5dfef94fbad4e187906c02d2eb862b2
Christian Engen <chris.a.engen@gmail.com>
2023-09-10 08:19:18 +0200

    scale more stuff

--- src/index.ai.js
+++ src/index.ai.js
@@ -13,27 +13,31 @@ var canvas = document.querySelector("canvas"),
   ballVelY = new Date() & 1 ? height * ballFract : -height * ballFract,
   p1 = 0,
   p2 = 0,
-  hundred = 100,
+  playerWidth,
   fifty = 50,
   ten = 10,
+  ballDim,
   fifteen = 15,
+  hundred = 100,
   min = Math.min,
   max = Math.max,
   raf = this.requestAnimationFrame,
   listener = this.addEventListener,
   context = canvas.getContext("2d");
 player1X = player2X = ballX = width / 2 - fifty;
-context.font = `40px Arial`;
 
-listener("resize", () => {
+let init = () => {
   width = canvas.width = this.innerWidth;
   height = canvas.height = this.innerHeight;
-});
+  playerWidth = width / 8;
+  ballDim = width / 80;
+  context.font = `40px Arial`;
+};
+init();
+listener("resize", () => init());
 let keyMap = {};
 var keyboardListener = (e) => {
   keyMap[e.keyCode] = e.type == "keydown";
-
-  console.log("keyMap in listener", keyMap);
 };
 listener("keydown", keyboardListener);
 listener("keyup", keyboardListener);
@@ -51,9 +55,13 @@ let collision = () => {
     ballX = width / 2;
     ballY = height / 2;
   }
-  if (ballX > player1X && ballX < player1X + hundred && ballY <= ten)
+  if (ballX > player1X && ballX < player1X + playerWidth && ballY <= ten)
     ballVelY = min(ballVelY * -1.1, ten);
-  if (ballX > player2X && ballX < player2X + hundred && ballY >= height - 25)
+  if (
+    ballX > player2X &&
+    ballX < player2X + playerWidth &&
+    ballY >= height - 25
+  )
     ballVelY = min(ballVelY * -1.1, ten);
 };
 
@@ -62,8 +70,8 @@ let last = undefined;
 let s = (now) => {
   if (!last) last = now;
   delta = now - last;
+
   if (delta > 1000 / 60) {
-    console.log("keyMap in step", keyMap);
     if (keyMap[37]) player1Vel -= fifteen;
     if (keyMap[39]) player1Vel += fifteen;
     if (ballVelY > 0) {
@@ -75,19 +83,20 @@ let s = (now) => {
       }
     }
 
-    player1X = min(max(player1X + player1Vel, 0), width - hundred);
-    player2X = min(max(player2X + player2Vel, 0), width - hundred);
+    player1X = min(max(player1X + player1Vel, 0), width - playerWidth);
+    player2X = min(max(player2X + player2Vel, 0), width - playerWidth);
     collision();
 
     ballX += ballVelX;
     ballY += ballVelY;
     context.clearRect(0, 0, width, height);
     context.fillStyle = "#000";
-    context.fillRect(player1X, 0, hundred, ten);
-    context.fillRect(player2X, height - 20, hundred, ten);
-    context.fillRect(ballX, ballY, ten, ten);
+    context.fillRect(player1X, 0, playerWidth, ten);
+    context.fillRect(player2X, height - 20, playerWidth, ten);
+    context.fillRect(ballX, ballY, ballDim, ballDim);
     context.fillStyle = "#777";
-    context.fillText(p1, width / 2 - ten, hundred);
+
+    context.fillText(p1, width / 2 - ten, hundred, hundred);
     context.fillText(p2, width / 2 - ten, height - hundred);
     player1Vel = player2Vel = 0;
     last = now;
